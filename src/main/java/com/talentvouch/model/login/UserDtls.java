259b14dc3400ae874d2e5853ba2a16e08b0afa4c
Ramana <>
2015-06-03 21:58:42 +0530

    Model Object for user details

---
+++ src/main/java/com/talentvouch/model/login/UserDtls.java
@@ -0,0 +1,146 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.talentvouch.model.login;
+
+/**
+ *
+ * @author Ramana
+ */
+public class UserDtls {
+    private String userName;
+    private String displayName;
+    private String userID;
+    private String userRoleId;
+    private String status;
+    
+    private String userDtlsId;
+    private String address;
+    private String phoneNo;
+    private String email;
+    private String rewardAmount;
+     private String experience;
+    private String jobTitle;
+    private String jobLocation;
+    private String skills;
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getUserRoleId() {
+        return userRoleId;
+    }
+
+    public void setUserRoleId(String userRoleId) {
+        this.userRoleId = userRoleId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getUserDtlsId() {
+        return userDtlsId;
+    }
+
+    public void setUserDtlsId(String userDtlsId) {
+        this.userDtlsId = userDtlsId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRewardAmount() {
+        return rewardAmount;
+    }
+
+    public void setRewardAmount(String rewardAmount) {
+        this.rewardAmount = rewardAmount;
+    }
+
+    public String getExperience() {
+        return experience;
+    }
+
+    public void setExperience(String experience) {
+        this.experience = experience;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public String getJobLocation() {
+        return jobLocation;
+    }
+
+    public void setJobLocation(String jobLocation) {
+        this.jobLocation = jobLocation;
+    }
+
+    public String getSkills() {
+        return skills;
+    }
+
+    public void setSkills(String skills) {
+        this.skills = skills;
+    }
+    
+    
+    
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+    
+    
+    
+}
